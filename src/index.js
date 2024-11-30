3d2c3d4467ca9841ee81609577910100b6559254
NgKhao <bsbnk141@gmail.com>
2024-11-30 20:59:35 +0700

    use Babel to compile Js than old version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+import "core-js/modules/es.object.values";
 import sum from "./utils";
 import "./styles/style.css";
 import "./styles/style.scss";
 console.log(sum(100, 10));
+
+// ES6 Spread Operator
+const person = { name: "Duoc" };
+const personClone = { ...person };
+console.log("personClone", personClone);
+
+// ES7 Object.values
+console.log("Object.values", Object.values(personClone));
