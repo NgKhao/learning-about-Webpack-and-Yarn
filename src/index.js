5a065769389e24741ec00e4a03e488dc98ad4644
NgKhao <bsbnk141@gmail.com>
2024-12-01 18:11:25 +0700

    use asset in webpack

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import "core-js/modules/es.object.values";
 import sum from "./utils";
 import "./styles/style.css";
 import "./styles/style.scss";
+import domHandler from "./dom";
 console.log(sum(100, 10));
+
+domHandler();
 
 // ES6 Spread Operator
 const person = { name: "Duoc" };
